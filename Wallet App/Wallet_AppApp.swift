05415c05e460d43a7c527d453ba2ed570dab2fc1
Jessica Soares <jess.soares@hotmail.com>
2023-11-03 09:46:14 +0000

    Initial Commit

---
+++ Wallet App/Wallet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_AppApp.swift
+//  Wallet App
+//
+//  Created by Jessica Soares on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
